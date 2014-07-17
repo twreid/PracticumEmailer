b3c1892c920fba1756171553a393a064174da353
Tyler Reid <reid.tylerw@gmail.com>
2014-07-16 23:01:18 -0500

    Fixed the Course Edit control.

--- PracticumEmailer/EditClearances.xaml.cs
+++ PracticumEmailer/EditClearances.xaml.cs
@@ -1,51 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Windows;
 
 namespace PracticumEmailer
 {
     /// <summary>
-    /// Interaction logic for EditClearances.xaml
+    ///     Interaction logic for EditClearances.xaml
     /// </summary>
     public partial class EditClearances : Window
     {
-        private BindingList<Course> courses;
-
-        
+        private readonly BindingList<Course> _courses;
 
         public EditClearances()
         {
             InitializeComponent();
-            List<Course> c = CourseManager.getCourseList();
-            c.Sort(SortCourses);
-            courses = new BindingList<Course>(c);
-            
 
-            dataGrid1.DataContext = courses;           
-            
+            List<Course> courses = CourseManager.getCourseList().OrderBy(c => c.CourseId).ToList();
+
+            _courses = new BindingList<Course>(courses);
+
+            CourseGrid.DataContext = _courses;
         }
 
-        void EditClearances_Closing(object sender, CancelEventArgs e)
+        private void EditClearances_Closing(object sender, CancelEventArgs e)
         {
-            //MessageBox.Show("Window Closing");
-
-            CourseManager.saveCourses(new List<Course>(courses));
+            CourseManager.saveCourses(new List<Course>(_courses));
         }
-
-        public int SortCourses(Course c1, Course c2)
-        {
-            return c1.CourseId.CompareTo(c2.CourseId);
-        }
-
-       }
+    }
 }
