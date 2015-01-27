eee4c202f0dceae095fcd5f3402f2efd77708fc3
Tyler Reid <reid.tylerw@gmail.com>
2015-01-26 21:40:52 -0600

    CLEANUP-Started working on the Template editor.

--- PracticumEmailer.Ui/ViewModels/EditTemplatesViewModel.cs
+++ PracticumEmailer.Ui/ViewModels/EditTemplatesViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +12,20 @@ namespace PracticumEmailer.Ui.ViewModels
 
     public class EditTemplatesViewModel : Screen
     {
+        private readonly string _templatesPath =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                Properties.Settings.Default.TemplateDirectory);
+        private readonly BindableCollection<string> _files;
+
+        [ImportingConstructor]
+        public EditTemplatesViewModel()
+        {
+            var info = new DirectoryInfo(_templatesPath);
+            _files =
+                new BindableCollection<string>(
+                    info.EnumerateFiles("*.html").Select(fi => fi.Name.Remove(fi.Name.IndexOf('.')).ToUpper()));
+        }
+
+        public BindableCollection<String> Files { get { return _files; } }
     }
 }
